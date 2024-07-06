56802cb8d64932478de7184c49590ff71d9e4521
yunnb <chldbsrud010@naver.com>
2024-07-06 21:58:14 +0900

    06. 조건부 렌더링

--- src/App.js
+++ src/App.js
@@ -6,10 +6,11 @@ import Wrapper from "./chapter_01/05/Wrapper";
 function App() {
   return (
     <Wrapper>
-      <Hello name="react" color="red"></Hello>
+        {/*isSpecial = {true}: true 는 js 값이므로 중괄호로 감싸줌
+            isSpecial: 값 설정 생략 시 true 로 간주 */}
+      <Hello name="react" color="red" isSpecial></Hello>
       <Hello color="pink"></Hello>
     </Wrapper>
-
   );
 }
 
--- src/chapter_01/05/Hello.js
+++ src/chapter_01/05/Hello.js
@@ -2,7 +2,12 @@ import React from "react"
 
 function Hello(props) {
     return (
-        <div style={{color: props.color}}>Hello {props.name}</div>
+        <div style={{color: props.color}}>
+            {/* 단순히 조건이 t/f에 따라 보여주고 숨겨주는 기능이라면 && 연산자가 더 간편함
+                   isSpecial: false -> false, true -> <b>*</b> */}
+            { props.isSpecial && <b>*</b>}
+            Hello {props.name}
+        </div>
     );
 }
 
